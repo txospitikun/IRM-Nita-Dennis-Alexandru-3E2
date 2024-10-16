3a7ed2200ef7277a8b60fb397c6c83f707c1a170
chinezprost <playschan@gmail.com>
2024-10-17 02:49:24 +0300

    added demo mp4 for hw 1

---
+++ Laborator_2_Tema/Assets/AttachLogic.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachLogic : MonoBehaviour
+{
+    public GameObject AttachHand;
+
+    public GameObject AttachableObject;
+
+    public Transform offsetRotation;
+
+    public void SetAttachableTransform()
+    {
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
